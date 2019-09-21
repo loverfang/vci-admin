2560c42219a9576ef79c97e5d29b2132b7e9ade5
luozhongxue <249590716@qq.com>
2019-09-21 18:00:52 +0800

    进入到后台但是报错,待解决！

--- src/utils/request.js
+++ src/utils/request.js
@@ -14,12 +14,11 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['EC-Token'] = getToken()
     }
     return config
   },
@@ -47,7 +46,7 @@ service.interceptors.response.use(
 
     alert("最先返回:" + res.flag);
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.flag !== 1) {
       Message({
         message: res.message || 'Error',
         type: 'error',
